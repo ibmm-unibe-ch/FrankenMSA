3343f5c8010167bdc62af6810a567f95b6473faa
NoahHenrikKleinschmidt <noah.kleinschmidt@unibe.ch>
2025-01-31 16:32:24 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frankenfold",
+    version="0.1.0",
+    description="Frankenfold is a package for protein design and folding",
+    author="Jannik Gut, Noah Kleinschmidt, Thommas Lemmin",
+    author_email="jannik.gut@unibe.ch, noah.kleinschmidt@unibe.ch, thommas.lemmin@unibe.ch",
+    url="https://github.com/ibmm-unibe-ch/frankenfold-ui",
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+        "numpy",
+        "pathlib",
+        "pandas",
+        "loguru",
+    ],
+    extras_require={
+        "filter": [
+            "hh-suite",
+        ],
+        "torch": [
+            "torch",
+        ],
+        "docs": [
+            "sphinx",
+            "sphinx_rtd_theme",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Protein Design",
+    ],
+    python_requires=">=3.8",
+)
