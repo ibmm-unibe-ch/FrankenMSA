08c642de497274501447376a1dd498506423675d
Noah Kleinschmidt <Noah.kleinschmidt@unibe.ch>
2025-05-08 11:09:06 +0200

    update assets

---
+++ app/assets/animated_background.js
@@ -0,0 +1,34 @@
+new FinisherHeader({
+  "count": 4,
+  "size": {
+    "min": 1100,
+    "max": 1300,
+    "pulse": 0
+  },
+  "speed": {
+    "x": {
+      "min": 0.1,
+      "max": 0.3
+    },
+    "y": {
+      "min": 0.1,
+      "max": 0.3
+    }
+  },
+  "colors": {
+    "background": "#2e8d7b",
+    "particles": [
+      "#2a7065",
+      "#00af92"
+    ]
+  },
+  "blending": "overlay",
+  "opacity": {
+    "center": 1,
+    "edge": 0.1
+  },
+  "skew": -2,
+  "shapes": [
+    "c"
+  ]
+});
