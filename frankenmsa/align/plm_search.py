efed83a77837f2d9f8a035e63fb32c0db5c1071f
rostro36 <jannik.gut@unibe.ch>
2026-01-26 17:18:17 +0100

    different import?

--- frankenmsa/align/plm_search.py
+++ frankenmsa/align/plm_search.py
@@ -12,7 +12,7 @@ import requests
 import pandas as pd
 
 from . import base
-from ..utils.uniprot import fetch_uniprot_metadata
+from ..utils import uniprot
 
 PLM_SEARCH_URL = "https://dmiip.sjtu.edu.cn/PLMSearch/submit/"
 BOUNDARY = (
@@ -178,7 +178,7 @@ class PLMSearch(base.MSAFactory):
         valid_df = df[df["similarity"] >= similarity_cutoff].copy()
         with open("test.txt", "a") as myfile:
             myfile.write(f"valid_df: {valid_df}")
-        uniprot_out = fetch_uniprot_metadata(valid_df["response"].to_list())
+        uniprot_out =  uniprot.fetch_uniprot_metadata(valid_df["response"].to_list())
         with open("test.txt", "a") as myfile:
             myfile.write(f"uniprot_out: {uniprot_out}")
         uniprot_df = pd.DataFrame([{"id":ide,"sequence":uniprot_out[ide]["sequence"] } for ide in uniprot_out.keys()] )
